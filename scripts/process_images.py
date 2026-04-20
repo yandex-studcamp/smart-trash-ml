c76072a0c66171d51ae4f74999ed8a7489eff6be
Matvey_Emelkov <471918@edu.itmo.ru>
2026-04-20 16:56:45 +0300

    feat: скачивание датасета с HF

--- scripts/process_images.py
+++ scripts/process_images.py
@@ -2,10 +2,11 @@ import os
 import shutil
 from pathlib import Path
 
+import imagehash
 from PIL import Image
 from tqdm import tqdm
 
-from configs.data_config import DIR_OUT_IMAGES, DIR_RESIZED, DIR_REALWASTE, TARGET_SIZE, CLASS_MAPPING
+from configs.data_config import DIR_OUT_IMAGES, DIR_RESIZED, DIR_REALWASTE, TARGET_SIZE, CLASS_MAPPING, DIR_HF
 
 
 def crop_center_and_resize(img, size):
@@ -23,8 +24,17 @@ def main():
     for cls in set(CLASS_MAPPING.values()) | {'other'}:
         os.makedirs(DIR_OUT_IMAGES / cls, exist_ok=True)
 
+    # Хранилище уже добавленных хешей
+    seen_hashes = set()
+
     all_files = []
-    for ds_name, ds_path in [("resized", DIR_RESIZED), ("realwaste", DIR_REALWASTE)]:
+    datasets = [
+        ("resized", DIR_RESIZED),
+        ("realwaste", DIR_REALWASTE),
+        ("hf", DIR_HF)
+    ]
+
+    for ds_name, ds_path in datasets:
         if not ds_path.exists(): continue
         for root, _, files in os.walk(ds_path):
             for file in files:
@@ -34,13 +44,22 @@ def main():
     for ds_name, folder_path, file_name in tqdm(all_files, desc="Обработка фото"):
         target_class = CLASS_MAPPING.get(folder_path.name, 'other')
         src_file = folder_path / file_name
-        new_name = f"{ds_name}_{folder_path.name}_{file_name}"
-        target_file = DIR_OUT_IMAGES / target_class / new_name
 
         try:
             with Image.open(src_file) as img:
                 if img.mode != 'RGB': img = img.convert('RGB')
+
+                img_hash = imagehash.phash(img)
+
+                if img_hash in seen_hashes:
+                    continue
+
+                seen_hashes.add(img_hash)
+
+                new_name = f"{ds_name}_{folder_path.name}_{file_name}"
+                target_file = DIR_OUT_IMAGES / target_class / new_name
                 crop_center_and_resize(img, TARGET_SIZE).save(target_file, 'JPEG', quality=90)
+
         except Exception as e:
             print(f"\nОшибка {src_file}: {e}")
 
